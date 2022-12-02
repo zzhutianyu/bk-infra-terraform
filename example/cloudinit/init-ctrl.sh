6066145d199efae123a6bf4ab022704a1567c86c
zhutianyu <704735206@qq.com>
2022-12-02 18:42:57 +0800

    ctrl to module

--- example/cloudinit/init-ctrl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# follow https://bk.tencent.com/docs/document/7.0/172/29311
-
-export HOME="/root"
-yum install -y jq unzip uuid
-curl -fsSL https://bkopen-1252002024.file.myqcloud.com/ce7/bcs.sh | bash -s -- -i k8s
-curl -sSf https://bkopen-1252002024.file.myqcloud.com/ce7/7.0-stable/bkdl-7.0-stable.sh | bash -s -- -ur latest base cert
